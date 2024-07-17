4f88c8b63ea1648ba14077ee3f7eedeb90efc159
unknown <minhmvpd09444@fpt.edu.vn>
2024-07-18 03:12:57 +0700

    them dau cham fay

--- AsigMent_c#5_MaiVanMinh_Pd09444/obj/Debug/net8.0/AsigMent_c#5_MaiVanMinh_Pd09444.AssemblyInfo.cs
+++ AsigMent_c#5_MaiVanMinh_Pd09444/obj/Debug/net8.0/AsigMent_c#5_MaiVanMinh_Pd09444.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AsigMent_c#5_MaiVanMinh_Pd09444")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc9653150bfe668630d8f29a7a587af8971ac30")]
 [assembly: System.Reflection.AssemblyProductAttribute("AsigMent_c#5_MaiVanMinh_Pd09444")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AsigMent_c#5_MaiVanMinh_Pd09444")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
